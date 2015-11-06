d410bbcb2a4835df096e3c408ae8a0630485d258
Ezequiel Tomas Lynch <elynch@lab110.it.itba.edu.ar>
2015-11-06 19:06:37 -0300

    piano fix, itunes completed

--- Kernel/keyboard.c
+++ Kernel/keyboard.c
@@ -63,7 +63,7 @@ void write_key(char scancode){
       boolSelector = 1;
     }
   }
-  if(!keyboardActivated && ((0xFFFFFFA0 <= scancode) && (scancode <= 0xFFFFFFA6) || 0xFFFFFF9E == scancode))
+  if(!keyboardActivated && ((0xFFFFFFA0 <= scancode) && (scancode <= 0xFFFFFFA6)))
     stopSounderC();
 
   if(counter == 250)
