6cd74bbc83cc5228fd8ba59a1fd14924709dfdda
nico <nicodonof@hotmail.com>
2016-07-04 17:41:53 -0300

    asdasdasd

--- Userland/SampleCodeModule/game.c
+++ Userland/SampleCodeModule/game.c
@@ -43,10 +43,12 @@ void game_sound(){
 	void* soundQ;
 	char* msg;
 	syscaller(17,0, "gameaudioq",0, &soundQ);//syscall start queue named gameaudioq
-	while(1){
-		while(msg == 0){
+
+    while(1){
+		while(*msg == 0){
 			syscaller(21,0, &soundQ,0, &msg); //syscall get message from soundQ
 		}
+        putChar(*msg);
 		switch(*msg){
             case 'b':
             	//syscall beep
@@ -61,7 +63,7 @@ void game_sound(){
             	//syscall pause song
             break;                            
         } 
-        msg = 0;
+        *msg = 0;
     }
 
     return;
@@ -80,7 +82,7 @@ void game_render(){
     syscaller(6,0,&timeStart,0,0);  // syscall que pide timeStart
     long timeNow;
 
-    syscaller(20, 'm', &soundQ, 0, 0);		//syscall send "start music (m)" to gameaudioq 
+    //syscaller(20, 'm', &soundQ, 0, 0);		//syscall send "start music (m)" to gameaudioq 
 
     int random_seed;
 
@@ -141,7 +143,7 @@ void game_render(){
         // syscaller(3,0,0,0,0);       //clearScreen   
         // printGame();
         
-        printBoard(board);
+        //printBoard(board);
 
     //----------------------GUIDO----------------------------------
 
